a3e9afc65f0c1384529c2d6af03edb19d2b478ea
Kowtha Saketh <kowtha.saketh@outlook.com>
2020-02-03 12:04:18 +0530

    Workspace config

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 /**
+ *   
  * @author Kowtha Saketh
  * @description Fundoo App  root files
  */
 
-import React from 'react';
+ import React from 'react';
 import './index.scss'
 import {
-    Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
+	Router,
+	Switch,
+	Route,
+} from "react-router-dom";
 
 import Login from './src/scenes/Login/Login'
 import Dashboard from './src/scenes/Dashboard/Dashboard'
@@ -25,8 +26,8 @@ const history = createHistory();
 
 //History listener to update language param in URL
 history.listen((location, action) => {
-  if(window.location.href.indexOf("Dashboard") == -1)
-    window.history.pushState({}, null, "?lang="+(localStorage.getItem('lang') || 'en'))
+	if(window.location.href.indexOf("Dashboard") == -1)
+		window.history.pushState({}, null, "?lang="+(localStorage.getItem('lang') || 'en'))
 })
 
 /**
@@ -34,20 +35,20 @@ history.listen((location, action) => {
  * @description Root Component of this project routes are declared here
  */
 export default class App extends React.Component {
-    render() {
-        return (
-            <Router history={history} basename={"/"}>
-              <Switch>
-                <Route path="/login" component={() => <Login view={LOGIN}/>} />
-                <Route path="/signup" component={() => <Login view={SIGNUP}/>} />
-                <Route path="/resetpassword" component={() => <Login view={RESETPASSWORD} />} />
-                <Route path="/Dashboard" component={Dashboard} />
-                <Route exact path="/">
-                  {() => history.push("/login")}
-                </Route>
-                <Route component={NotFound}/>
-              </Switch>
-          </Router>
-        )
-    }
+		render() {
+				return (
+						<Router history={history} basename={"/"}>
+							<Switch>
+								<Route path="/login" component={() => <Login view={LOGIN}/>} />
+								<Route path="/signup" component={() => <Login view={SIGNUP}/>} />
+								<Route path="/resetpassword" component={() => <Login view={RESETPASSWORD} />} />
+								<Route path="/Dashboard" component={Dashboard} />
+								<Route exact path="/">
+									{() => history.push("/login")}
+								</Route>
+								<Route component={NotFound}/>
+							</Switch>
+					</Router>
+				)
+		}
 }
